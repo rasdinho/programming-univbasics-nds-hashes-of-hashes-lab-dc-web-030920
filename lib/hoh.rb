9c6bb460853109b62d773a03de312a29afa0c8b3
Ras azzab <ras.dinho7@gmail.com>
2020-02-27 04:48:28 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -57,3 +57,30 @@
 # }
 
 # end
+def naming_system
+   family = {
+  :label => "Family",
+  :sub_category => {
+    :label => "Genus",
+    :sub_category => {
+        :label => "Species",
+  :sub_category => nil
+    }
+  }
+}
+   order = {
+   :label => "Order", 
+   :sub_category => family
+ }
+    domain = {
+  :label => "Kingdom",
+   :sub_category => {
+     :label => "Phylum",
+     :sub_category => {
+       :label => "Class",
+        :sub_category => order 
+    }
+  }
+}
+  domain
+end
