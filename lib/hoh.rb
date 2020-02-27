28642260795ce4279319947cc34acb55769f705f
Ras azzab <ras.dinho7@gmail.com>
2020-02-27 04:29:12 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -45,8 +45,9 @@ def naming_system
   #  Kingdom
   #  Phylum
   #  Class
-  #  Order
   #  Family
+  # # pro_3={
+  # sub_category:"order"
   #  Genus
   #  Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
