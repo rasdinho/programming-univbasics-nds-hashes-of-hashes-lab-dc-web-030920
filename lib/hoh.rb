bbe9d7a0ebce605211a807213b96ae6d8155f81f
Ras azzab <ras.dinho7@gmail.com>
2020-02-27 04:21:57 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,6 +31,16 @@
 # }
 
 def naming_system
+  PORTION_1 = {
+  label: "Kingdom",
+  sub_category: {
+    label: "Phylum",
+    sub_category: {
+      label: "Class"
+    }
+  }
+}
+
   # Remember:
   #  Kingdom
   #  Phylum
